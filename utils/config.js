e0c3fc5737fc3b677240eacce22e64f2f682ef02
Olayemi Oyebanji <olayemioyebanji911@gmail.com>
2020-08-30 17:56:57 +0100

    update pdf

--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,5 @@
 export const config = {
   apiUrl: 'https://qps-backend.herokuapp.com/api',
+  paystackPkey: 'pk_test_a4f7333be99a7c59da70a2084577a4d4b35243fc',
+  paystackSKey: 'sk_test_95e0ad209100b755d9c2ec23eadca163178cd3c5',
 };
